1f1c47c8a4d22bde3484560d22cad8a18c6faa58
ron19907 <ronaldmweema@gmail.com>
2025-07-15 00:48:14 +0200

    used a card component in the landing page

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -5,7 +5,7 @@ interface PillProps {
 const Pill: React.FC<PillProps> = ({ title }) => {
     return (
         <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-            <p className=" text-sm ">{title}</p>
+            <p className=" text-sm text-gray-800 ">{title}</p>
         </div>
     )
 }
